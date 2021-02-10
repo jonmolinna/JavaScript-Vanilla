2aa631a58f4ee7254a067e739c76ee254f8cea3c
jonmolinna <jmolina2624@gmail.com>
2021-02-10 16:22:26 -0500

    app.js

---
+++ 4.-navbar/app.js
@@ -0,0 +1,20 @@
+// classlist - shows/gets all classes
+// contains - checks classlist for specific class
+// add - add class
+// remove - remove class
+// toggle - toggles class
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function(){
+    //console.log(links.classList.contains("links")); // consulta
+    
+    // if(links.classList.contains('show-links')){
+    //     links.classList.remove('show-links')
+    // } else {
+    //     links.classList.add("show-links");
+    // }
+
+    links.classList.toggle("show-links");
+});
